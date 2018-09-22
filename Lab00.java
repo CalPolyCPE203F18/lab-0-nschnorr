bfa2180df2aa8511afb306b381c614d6c50a2246
Nick Schnorr <nschnorr@calpoly.edu>
2018-09-22 13:22:26 -0700

    commit

--- Lab00.java
+++ Lab00.java
@@ -2,7 +2,35 @@ public class Lab00
 {
    public static void main(String[] args)
    {
-
-   
+       //declaring and initializing some variables
+       int x = 5;
+       String y = "hello";
+       double z = 9.8;
+       // printing the variables
+       System.out.println("x: " + x + " y: " + y + " z: " + z);
+       // a list (make an array in java)
+       int[] nums = {3, 6, -1, 2};
+       for (int num : nums)
+           System.out.println(num);
+       // call a function
+       int numFound = charCount(y,'l');
+       System.out.println("Found: " + numFound);
+       // a counting for loop
+       for (int i = 0; i < 11; i++)
+           System.out.print(i + " ");
+       System.out.println();
    }
+    public static int charCount(String s, char c)
+    {
+        int count = 0;
+        char[] charArray = s.toCharArray();
+        for (char ch : charArray)
+        {
+            if (ch == c)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
 }
